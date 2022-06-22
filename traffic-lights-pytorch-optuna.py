1a97fa9496711d53876bb1d49322386cc6de3879
lc3267 <melvin.strobl@kit.edu>
2022-06-22 13:54:43 +0200

    moved to seperate file

--- traffic-lights-pytorch-optuna.py
+++ traffic-lights-pytorch-optuna.py
@@ -3,60 +3,17 @@ import torch as t
 import numpy as np #interp, repeat
 import optuna as o
 
-# %%
-x_base = t.FloatTensor([[0,0,1], [0,1,0], [1,1,0], [1,0,0]])
-y_base = t.FloatTensor([1, 1, 0, 0])
+from traffiq import generate_data, traffic_mlp
 
-modes = {'train':0.70, 'valid':0.20, 'test':0.10}
-scatter = 0.6
-features = 3
-augmentation_size = 100
-datasets = {mode:None for mode in modes.keys()}
+import logging
 
-for mode, proportion in modes.items():
-    actual_augmentation = int(augmentation_size*proportion)
+augmentation_size=100
+scatter=0.6
+modes={'train':0.70, 'valid':0.20, 'test':0.10}
 
-    x_enhanced = t.zeros((actual_augmentation*x_base.size(0), features))
-    y_enhanced = np.repeat(y_base, augmentation_size*proportion)
+datasets = generate_data(augmentation_size, scatter, modes)
 
-    for b, c in enumerate(x_base):
-        for a in range(actual_augmentation):
-            random_light = scatter/2-scatter*t.rand(features)
-            scattered_feature = c + random_light
-            x_enhanced[b*actual_augmentation + a] = t.FloatTensor(np.interp(scattered_feature, (scattered_feature.min(), scattered_feature.max()), (0, +1) ))
-
-    datasets[mode] = t.utils.data.TensorDataset(x_enhanced, y_enhanced)
-
-    print(f"{mode}-dataset size: {x_enhanced.shape}")
-
-
-# %%
-class MLP(t.nn.Module):
-    def __init__(self, input_dim, output_dim, arch=[4,2]):
-        super(MLP, self).__init__()
-        self.input_layer = t.nn.Linear(input_dim, arch[0])
-
-        self.hidden_layers = []
-        for layer_it in range(len(arch)-1):
-            self.hidden_layers.append(t.nn.Linear(arch[layer_it], arch[layer_it+1]))
-
-        self.output_layer = t.nn.Linear(arch[-1], output_dim)
-        
-    def forward(self, x):
-        x = t.nn.functional.relu(self.input_layer(x))
-
-        for hidden_layer in self.hidden_layers:
-            x = t.nn.functional.relu(hidden_layer(x))
-
-        x = t.sigmoid(self.output_layer(x))
-        return x
-
-# %%
-epochs = 100
 batch_size = 10
-
-# model = MLP(features, 1)
-
         
 dataloaders = {mode:None for mode in modes}
 dataloaders["train"] = t.utils.data.DataLoader(datasets["train"], batch_size=batch_size, shuffle=True)
