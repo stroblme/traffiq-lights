159556aece1e8379679b7881a954113092b4a3cd
lc3267 <melvin.strobl@kit.edu>
2022-06-23 15:33:05 +0200

    added pqc

--- traffiq.py
+++ traffiq.py
@@ -1,6 +1,6 @@
 import torch as t
 import numpy as np #repeat
-
+import qiskit as q
 import logging
 
 def generate_data(augmentation_size:int=100, scatter:float=0.6, modes:dict={'train':0.70, 'valid':0.20, 'test':0.10}
@@ -53,3 +53,63 @@ class traffic_mlp(t.nn.Module):
 
         x = t.sigmoid(self.output_layer(x))
         return x
+
+class traffiq_pqc():
+    def __init__(self, input_dim, output_dim, arch=[2, 2], shots=1024):
+        self.shots = shots
+        
+        self.enc_qc = q.circuit.library.ZZFeatureMap(feature_dimension=input_dim, reps=arch[0])
+        self.var_qc = q.circuit.library.TwoLocal(input_dim, ['ry', 'rz'], 'cz', reps=arch[1])
+
+        self.qc = self.enc_qc.compose(self.var_qc)
+        self.qc.measure_all()
+
+        self.qc.draw()
+
+    def circuit_parameters(self, data, variational):
+        parameters = {}
+        for i, p in enumerate(self.enc_qc.ordered_parameters):
+            parameters[p] = data[i]
+        for i, p in enumerate(self.var_qc.ordered_parameters):
+            parameters[p] = variational[i]
+        return parameters
+
+    def assign_label(self, bitstring):
+        hamming_weight = sum([int(k) for k in list(bitstring)])
+        odd = hamming_weight & 1
+        if odd:
+            return 0
+        else:
+            return 1
+
+    def label_probability(self, results):
+        shots = sum(results.values())
+        probabilities = {0: 0, 1: 0}
+        for bitstring, counts in results.items():
+            label = self.assign_label(bitstring)
+            probabilities[label] += counts / shots
+        return probabilities
+
+    def classification_probability(self, data, variational):
+        circuits = [self.qc.assign_parameters(
+            self.circuit_parameters(d,variational)) for d in data]
+
+        backend = q.BasicAer.get_backend('qasm_simulator')
+        results =  q.execute(circuits, backend).result()
+
+        classification = [self.label_probability(results.get_counts(c)) for c in circuits]
+        return classification
+
+    def cross_entropy_loss(self, predictions, expected):
+        p = predictions.get(int(expected)) # need int-cast here for tensor incompatibility
+        return -(expected*np.log(p)+(1-expected)*np.log(1-p))
+    
+    def cost_function(self, data, labels, variational):
+        classifications = self.classification_probability(data, variational)
+
+        cost = 0
+        for i, classification in enumerate(classifications):
+            cost += self.cross_entropy_loss(classification, labels[i])
+        cost /= len(data)
+
+        return cost
